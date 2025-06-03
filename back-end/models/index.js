73f8323154752d8c73779b4fc80740b90b5011b2
Nguyen Huy Tam <tamnhhe@gmail.com>
2025-06-03 09:18:00 +0700

    Create index.js

---
+++ back-end/models/index.js
@@ -0,0 +1,31 @@
+// Nguyễn Đức Linh - HE170256 17/1/2025
+const mongoose = require("mongoose");
+const Notification = require("./notification.model");
+const User = require("./user.model");
+const Product = require("./product.model");
+const InventoryTransaction = require("./inventoryTransaction.model");
+const Category = require("./category.model");
+const SupplierProduct = require("./supplierProduct.model");
+const Supplier = require("./supplier.model");
+
+const db = {};
+
+db.User = User;
+db.Product = Product;
+db.InventoryTransaction = InventoryTransaction;
+db.Notification = Notification;
+db.Category = Category;
+db.SupplierProduct = SupplierProduct;
+db.Supplier = Supplier;
+
+db.connectDB = async () => {
+  try { 
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Kết thúc quá trình nếu có lỗi
+  }
+};
+
+module.exports = db;
