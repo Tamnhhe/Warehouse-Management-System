f9ab7ed2e5a1c477dca52e987d5e4d1fbde51c88
Nguyen Huy Tam <tamnhhe@gmail.com>
2025-06-03 09:17:57 +0700

    Create category.model.js

---
+++ back-end/models/category.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema({
+    categoryName: {
+        type: String,
+        required: true,
+    },
+    classifications: [
+        {
+            _id: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            description: {
+                type: String,
+            },
+        },
+    ],
+    status: {
+        type: String,
+        required: true,
+        enum: ['active', 'inactive'],
+        default: 'active',
+    },
+    description: {
+        type: String,
+    },
+});
+
+const Category = mongoose.model('Category', CategorySchema);
+module.exports = Category;
