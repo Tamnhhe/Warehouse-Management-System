ea4e749c63a3d2973a219c368fa9674a8de534cd
Nguyen Huy Tam <tamnhhe@gmail.com>
2025-06-03 09:19:33 +0700

    Update inventoryTransaction.model.js

---
+++ back-end/models/inventoryTransaction.model.js
@@ -0,0 +1,79 @@
+
+const mongoose = require('mongoose');
+
+const inventoryTransactionSchema = new mongoose.Schema({
+    supplier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Supplier", // Đảm bảo đúng model
+        required: true
+    },
+    transactionType: { //xác đinh xem xuất hay nhập
+        type: String,
+        required: true,
+        enum: ['import', 'export'],
+    },
+    transactionDate: { //Ngày xuất nhà cung cấp
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    products: [ //Danh sách sản phẩm
+        {
+            supplierProductId: {  //ID sản phẩm
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'SupplierProduct',
+            },
+            requestQuantity: { //So luong san pham mong muon
+                type: Number,
+                min: 0,
+            },
+            receiveQuantity: { //So luong san pham nhan dươc
+                type: Number,
+                min: 0,
+            },
+            defectiveProduct: { //So luong san pham loi
+                type: Number,
+                min: 0,
+            },
+            achievedProduct: { //So luong san pham chap nhan
+                type: Number,
+                min: 0,
+            },
+            price: { //Giá bán
+                type: Number,
+                min: 0,
+            },
+            expiry: { //nngaf heetts hạn
+                type: Date,
+            }
+        },
+    ],
+    operator: { //Nhân viên chiu trách  nhiệm cho đơn
+        type: mongoose.Schema.Types.ObjectId,
+        // required: true,
+        ref: 'User',
+    },
+    notification: { //Thong bao
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: 'Notification',
+    },
+    totalPrice: { //Tong tien
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    branch: { //Chi nhanh
+        type: String,
+    },
+    status: { //Trang thái
+        type: String,
+        required: true,
+        enum: ['pending', 'completed', 'cancelled'],
+        default: 'pending',
+    },
+}, { timestamps: true });
+
+const inventoryTransaction = mongoose.model('InventoryTransaction', inventoryTransactionSchema);
+module.exports = inventoryTransaction;
