53d2178b13e33311c1aa0ab05a2f40d512e8b783
Nguyen Huy Tam <tamnhhe@gmail.com>
2025-06-03 09:17:28 +0700

    Create index.js

---
+++ back-end/controller/index.js
@@ -0,0 +1,16 @@
+// Nguyễn Đức Linh - HE170256 18/1/2025
+const InventoryTransactionController = require("./inventoryTransaction.controller");
+const UserController = require("./user.controller");
+const ProductController = require("./product.controller");
+const NotificationController = require("./notification.controller");
+const CategoryController = require("./category.controller");
+const AuthenticationController = require("./authentication.controller");
+
+module.exports = {
+    InventoryTransactionController,
+    UserController,
+    ProductController,
+    NotificationController,
+    CategoryController,
+    AuthenticationController
+};
