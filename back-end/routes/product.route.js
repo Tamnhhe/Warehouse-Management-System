19fa2f1cad738006939d3c381ad37297ae6e4013
Nguyen Huy Tam <tamnhhe@gmail.com>
2025-07-03 17:10:38 +0700

    format

--- back-end/routes/product.route.js
+++ back-end/routes/product.route.js
@@ -5,20 +5,27 @@ const { ProductController } = require("../controllers");
 const multer = require("multer"); // Import Multer để upload file
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads'); // Đường dẫn tới thư mục lưu file upload
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname); // Tạo tên file mới không bị trùng
-    }
+  destination: function (req, file, cb) {
+    cb(null, "./uploads"); // Đường dẫn tới thư mục lưu file upload
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); // Tạo tên file mới không bị trùng
+  },
 });
 const upload = multer({ storage: storage });
 
 productRouter.get("/getAllProducts", ProductController.getAllProducts);
 productRouter.get("/getProductById/:id", ProductController.getProductById);
-productRouter.post("/createProduct",upload.single("productImage"), ProductController.createProduct);
-productRouter.put("/updateProduct/:id",upload.single("productImage"), ProductController.updateProduct);
+productRouter.post(
+  "/createProduct",
+  upload.single("productImage"),
+  ProductController.createProduct
+);
+productRouter.put(
+  "/updateProduct/:id",
+  upload.single("productImage"),
+  ProductController.updateProduct
+);
 productRouter.put("/inactivateProduct/:id", ProductController.inactiveProduct);
 
 module.exports = productRouter;
-
